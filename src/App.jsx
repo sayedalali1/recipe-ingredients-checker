38554ea557239073d742a796688410d3308824f4
sayedalali1 <sayedalali1@gmail.com>
2024-08-04 20:48:44 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 // src/App.jsx
+import './App.css';
 
 const App = () => {
+  const recipes = [
+    { ingredients: 'Salt', availabe: true },
+    { ingredients: 'Pepper', availabe: false },
+    { ingredients: 'Tumeric', availabe: true },
+  ];
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Recipes List</h1>
+      <ul>
+        {recipes.map((recipe, index) => (
+          <li key={index} className={recipe.availabe ? 'completed' : 'not-completed'}>
+            {recipe.ingredients}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
-export default App
+export default App;
